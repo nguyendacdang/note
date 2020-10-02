8af2874fd87db0cf8fadf27599e6069998d9e7a9
dangnd <dangnd@runsystem.net>
2020-10-02 17:36:29 +0700

    update

--- Code/test.py
+++ Code/test.py
@@ -4,17 +4,5 @@ import scipy.stats as stats
 import statsmodels.api as api
 
 
-def generate_autocorrelated_data(theta, mu, sigma, N):
-    X = np.zeros((N,1))
-    
-    for t in range(1,N):
-        X[t] = theta*X[t-1] + np.random.normal(mu, sigma)
-    return X
-
-sample_means = np.zeros(200-1)
-for i in range(1,200):
-    X = generate_autocorrelated_data(0.5, 0, 1 , i*10)
-    sample_means[i-1] = np.mean(X)
-
-plt.bar(range(1,200), sample_means)
-plt.show()
+stocks = np.array([1,2,3,4,5,6])
+print(stocks[:-1])
