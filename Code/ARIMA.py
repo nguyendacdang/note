9685e1d95b30a711efd6c92ddf5bcbe3d579db4c
dangnd <dangnd@runsystem.net>
2020-10-07 17:41:42 +0700

    update

---
+++ Code/ARIMA.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+import statsmodels.api as sm
+from statsmodels import regression
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import matplotlib.pyplot as plt
--- Code/test.py
+++ Code/test.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import statsmodels.api as api
+import call 
 
 
 stocks = np.array([1,2,3,4,5,6])
